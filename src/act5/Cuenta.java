8017732aa44320ba6b238d0c52d229bc215e4719
DarknessYamii <andresruizdelgado12@gmail.com>
2020-11-22 13:20:43 +0100

    Act 5 Commit

--- src/act5/Cuenta.java
+++ src/act5/Cuenta.java
@@ -10,6 +10,58 @@ public class Cuenta {
 		
 	}
 	
-	
+	public Cuenta(float DNI, float saldo, float interes) {
+		this.DNI = DNI;
+		this.saldo = saldo;
+		this.interes = interes;
+	}
+
+	public float getDNI() {
+		return DNI;
+	}
+
+	public long getNum_cuenta() {
+		return num_cuenta;
+	}
+
+	public void setDNI(float DNI) {
+		this.DNI = DNI;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+
+	public float getInteres() {
+		return interes;
+	}
+
+	public void setInteres(float interes) {
+		this.interes = interes;
+	}
+	public float actualizarSaldo() {
+		float nuevo_saldo;
+		nuevo_saldo = saldo + (saldo+((interes/100)/365));
+		return nuevo_saldo;
+	}
+	public double ingresarDinero(double ingresar) {
+		double nuevo_saldo;
+		nuevo_saldo = actualizarSaldo() + ingresar;
+		return nuevo_saldo;
+	}
+	public double retirarDinero(double retirar) {
+		double nuevo_saldo = 0 ;
+        if (this.saldo >= retirar) {
+            nuevo_saldo = actualizarSaldo() - retirar;
+        } else {
+            System.out.println("No hay saldo suficiente en su cuenta");
+        }
+        return nuevo_saldo;
+    }
+
 	
 }
---
+++ src/act5/funcionesBanco.java
@@ -0,0 +1,10 @@
+package act5;
+
+public class funcionesBanco {
+
+	public static void main(String[] args) {
+		Cuenta c = new Cuenta();
+		
+	}
+
+}
